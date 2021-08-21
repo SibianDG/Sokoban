7c95a051a3b96056db05c082a3fc24227c445ec9
SibianDG <sibian.degussem@student.hogent.be>
2021-08-21 14:37:06 +0200

    Changed connectionString

--- src/persistentie/Connectie.java
+++ src/persistentie/Connectie.java
@@ -1,5 +1,5 @@
 package persistentie;
 
 public class Connectie {
-    public static final String JDBC_URL = "jdbc:mysql://ID222177_g21.db.webhosting.be?serverTimezone=UTC&useLegacyDatetimeCode=false&user=ID222177_g21&password=plydsOp4";
+    public static final String JDBC_URL = "jdbc:mysql://localhost/ID222177_g21?serverTimezone=UTC&useLegacyDatetimeCode=false&user=root&password=root";
 }
